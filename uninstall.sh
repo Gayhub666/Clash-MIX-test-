4289a3050b303ba791dfa658a47484e00118fab3
Your Name <you@example.com>
2022-10-14 00:45:08 +0800

    从上一版复制一些东西

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,15 @@
 Clash_data_dir="/data/clash"
+Clash_delete_dir="/data/clash.delete"
 
 rm_data() {
+    mv ${Clash_data_dir} ${Clash_delete_dir}
     rm -rf ${Clash_data_dir}
     rm -rf /data/adb/service.d/clash_service.sh
+    rm -rf /data/data/xyz.chz.clash/
 }
 
 rm_data
-# UNINSTALL 
+
+#aria相关
+#killall -9 aria2c >/dev/null 2>&1
+#killall -9 lighttpd >/dev/null 2>&1
